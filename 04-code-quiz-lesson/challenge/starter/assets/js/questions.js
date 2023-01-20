4f67df1e743bff4c5c79234ffa06f44233a0f0ff
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-01-20 14:46:54 +0000

    adds questions array with base question object

---
+++ 04-code-quiz-lesson/challenge/starter/assets/js/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    Question1 = {
+        Question: 'This is Question1',
+        Answer1: 'This is Answer1',
+        Answer2: 'This is Answer2',
+        Answer3: 'This is Answer3',
+    },
+    Question2 = {
+        Question: 'This is Question2',
+        Answer1: 'This is Answer1',
+        Answer2: 'This is Answer2',
+        Answer3: 'This is Answer3',
+    },
+    Question3 = {
+        Question: 'This is Question3',
+        Answer1: 'This is Answer1',
+        Answer2: 'This is Answer2',
+        Answer3: 'This is Answer3',
+    },
+    Question4 = {
+        Question: 'This is Question4',
+        Answer1: 'This is Answer1',
+        Answer2: 'This is Answer2',
+        Answer3: 'This is Answer3',
+    },
+    Question5 = {
+        Question: 'This is Question5',
+        Answer1: 'This is Answer1',
+        Answer2: 'This is Answer2',
+        Answer3: 'This is Answer3',
+    },
+]
